3dae7ae8b319f173c1566205f9eb8e8858b6cc49
NithinReddyKumbham <nithinreddykumbham@gmai.com>
2022-04-21 15:19:36 -0500

    Added new files

---
+++ Kumbham_Movies/Kumbham_Movies/MovieCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieCollectionViewCell.swift
+//  Kumbham_Movies
+//
+//  Created by Kumbham,Nithin Reddy on 4/21/22.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+    
+    
+    
+}
